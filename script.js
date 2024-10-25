6556395b8854a26a09c4c5708c7aaa5817403dd5
laxmansinghkirar <149003046+laxmansinghkirar@users.noreply.github.com>
2024-10-25 22:21:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function scrollToAbout() {
+    document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+  }
+  
+  function scrollToContact() {
+    document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+  }
+  
